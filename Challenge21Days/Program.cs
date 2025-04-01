5aa0483ae94e240faa78df1edc73814178f05454
Kuba <Kuba@NBKBROZDA>
2025-04-01 21:12:06 +0200

    Update lession 4

--- Challenge21Days/Program.cs
+++ Challenge21Days/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Michal";
+char sex = 'M';
+int age = 17;
+
+if(sex == 'K' && age <30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat.");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30.");
+}
+else if(sex == 'M' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni meżczyzna.");
+}
+else
+{
+    Console.WriteLine("Żaden z powyższych warunków, nie został spełniony");
+}
