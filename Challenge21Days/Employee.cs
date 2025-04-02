177f7360fb733b030477afe3469aa0edf314d4c6
Kuba <Kuba@NBKBROZDA>
2025-04-02 15:08:03 +0200

    Lekcja 6

---
+++ Challenge21Days/Employee.cs
@@ -0,0 +1,28 @@
+﻿namespace Challenge21Days
+{
+    class Employee
+    {
+        List<int> Scores = new List<int>();
+
+        private int score = 0;
+        public string Login { get; private set; }
+        public string Password { get; private set; }
+        public int Result
+        {
+            get
+            {
+                return this.Scores.Sum();
+            }
+        }
+        public Employee(string login, string password)
+        {
+            this.Login = login;
+            this.Password = password;
+        }
+
+        public void AddGrade(int score)
+        {
+            this.Scores.Add(score);
+        }
+    }
+}
--- Challenge21Days/Program.cs
+++ Challenge21Days/Program.cs
@@ -1,20 +1,33 @@
-﻿string name = "Michal";
-char sex = 'M';
-int age = 17;
+﻿using Challenge21Days;
 
-if(sex == 'K' && age <30)
+Employee user1 = new Employee("Kuba", "oasu89346DG4$");
+Employee user2 = new Employee("Ewa", "qw23erty77u");
+Employee user3 = new Employee("Włodek", "asdttBB4@@4");
+
+int maxValue = int.MinValue;
+string userName = null;
+
+List<Employee> Users = new List<Employee>() { user1, user2, user3 };
+
+user1.AddGrade(5);
+user1.AddGrade(7);
+user1.AddGrade(3);
+
+user2.AddGrade(8);
+user2.AddGrade(7);
+user2.AddGrade(4);
+
+user3.AddGrade(9);
+user3.AddGrade(9);
+user3.AddGrade(10);
+
+foreach (var user in Users)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat.");
+    if (maxValue < user.Result)
+    {
+        maxValue = user.Result;
+        userName = user.Login;
+    }
 }
-else if (name == "Ewa" && age == 30)
-{
-    Console.WriteLine("Ewa, lat 30.");
-}
-else if(sex == 'M' && age < 18)
-{
-    Console.WriteLine("Niepełnoletni meżczyzna.");
-}
-else
-{
-    Console.WriteLine("Żaden z powyższych warunków, nie został spełniony");
-}
+
+Console.WriteLine($"Użytkownik: {userName}, uzyskał najwyższy wynik! Jest to: {maxValue}");
